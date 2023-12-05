987f987dbcaaf1b07ee9a6c5855624f554c495f2
effulgentsia <alex.bronstein@acquia.com>
2023-12-05 09:40:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import hyperscriptify from 'hyperscriptify';
 
-export function init({ components, propsify, h, Fragment, render }) {
+export function main({ components, propsify, h, Fragment, render }) {
   componentsWithFragment = { ...components, 'drupal-html-fragment': Fragment };
   document.querySelectorAll('template[hyperscriptify]').forEach((templateElement) => {
     const App = hyperscriptify(templateElement.content, h, Fragment, componentsWithFragment, { propsify });
