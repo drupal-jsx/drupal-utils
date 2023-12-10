e8aa12bd54ec5b1fabf0928520a5aa5e5b904f31
effulgentsia <alex.bronstein@acquia.com>
2023-12-09 18:25:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import hyperscriptify from '@drupal-jsx/hyperscriptify';
+import kebabCase from 'just-kebab-case';
+
+const dependencies = {};
 
 export function main({ components, propsify, h, Fragment, render }) {
+  dependencies.propsify = propsify;
   const componentsWithFragment = { ...components, 'drupal-html-fragment': Fragment };
   document.querySelectorAll('template[hyperscriptify]').forEach((templateElement) => {
     const App = hyperscriptify(templateElement.content, h, Fragment, componentsWithFragment, { propsify });
@@ -8,4 +12,12 @@ export function main({ components, propsify, h, Fragment, render }) {
     templateElement.after(container);
     render(App, container);
   });
+}
+
+export function props(attributes = {}) {
+  return dependencies.propsify(attributes, {}, {});
+}
+
+export function kebabCasePreserveDoubleDash(str) {
+  return str.split('--').map(kebabCase).join('--');
 }
